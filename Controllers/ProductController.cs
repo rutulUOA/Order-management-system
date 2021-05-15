bdc129f32dfad5a4f4c3f4a0b216debedcaa3650
Rutul Sunil Brahmankar <62677315+rutulUOA@users.noreply.github.com>
2021-05-15 17:50:59 +1200

    MVC files

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using sampleapp.context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sampleapp.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        RutulTrailEntities1 dbObj = new RutulTrailEntities1();
+        // GET: Product
+        public ActionResult Product()
+        {
+            return View("ProductOrder");
+        }
+
+        [HttpPost]
+        public ActionResult PlaceOrder(OrderTable model)
+        {
+            var datastock = dbObj.Product_table.Where(m => m.ID == model.Product_table.ID).FirstOrDefault().Items_In_Stock;
+            if (datastock <= 1)
+                ModelState.AddModelError("ProductID", "Product is out of stock");
+            if (ModelState.IsValid)
+            {
+                OrderTable obj = new OrderTable();
+                //var prodId = dbObj.Product_table.Where(m => m.Product_Name == model.Product_table.Product_Name).FirstOrDefault().ID;
+                obj.ProductID = model.Product_table.ID;
+                obj.UserID = model.UserTable.ID;
+                obj.Status = "ordered";
+                obj.Product_table = dbObj.Product_table.Where(m => m.ID == model.Product_table.ID).FirstOrDefault();
+                obj.UserTable = dbObj.UserTables.Where(m => m.ID == model.UserTable.ID).FirstOrDefault();
+                obj.Product_table.Items_In_Stock -= 1;
+                
+                if (model.ID == 0)
+                    dbObj.OrderTables.Add(obj);
+                else
+                    dbObj.Entry(obj).State = EntityState.Modified; 
+                dbObj.SaveChanges();
+                ViewData["NumbeOfItemsInStock"] = obj.Product_table.Items_In_Stock;
+                ModelState.Clear();
+            }
+            //ModelState.Clear();
+            return View("ProductOrder");
+        }
+
+        public ActionResult GetAllOrders()
+        {
+            var result = dbObj.OrderTables.ToList();
+            return View(result);
+        }
+
+        //public ActionResult 
+    }
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,88 @@
+﻿using sampleapp.context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sampleapp.Models
+{
+    public class Class1
+    {
+        public static SelectList GetProductNamesList()
+        {
+            using (var dbObj = new RutulTrailEntities1())
+            {
+                var result = dbObj.Product_table.ToList();
+
+                result = result.OrderBy(m => m.Product_Name).ToList();
+                List<SelectListItem> sl = result.ConvertAll(a =>
+                {
+                    return new SelectListItem()
+                    {
+                        Text = a.Product_Name,
+                        Value = a.ID.ToString()
+                    };
+                });
+                return new SelectList(sl, "Value", "text");
+            }
+        }
+
+        public static SelectList GetProductBrandList()
+        {
+            using (var dbObj = new RutulTrailEntities1())
+            {
+                var result = dbObj.Product_table.ToList();
+
+                result = result.OrderBy(m => m.Brand).ToList();
+                List<SelectListItem> sl = result.ConvertAll(a =>
+                {
+                    return new SelectListItem()
+                    {
+                        Text = a.Brand,
+                        Value = a.Brand
+                    };
+                });
+                return new SelectList(sl, "Value", "text");
+            }
+        }
+
+        public static SelectList GetProductTypeList()
+        {
+            using (var dbObj = new RutulTrailEntities1())
+            {
+                var result = dbObj.Product_table.ToList();
+                result = result.OrderBy(m => m.Product_Type).ToList();
+
+               List<SelectListItem> sl = result.ConvertAll(m =>
+               {
+                   return new SelectListItem()
+                   {
+                       Text = m.Product_Type,
+                       Value = m.Product_Type
+                   };
+               });
+            return new SelectList(sl, "Value", "Text");
+            }
+        }
+
+        public static SelectList GetUserNameList()
+        {
+            using (var dbObj = new RutulTrailEntities1())
+            {
+                var result = dbObj.UserTables.ToList();
+
+                result = result.OrderBy(m => m.UserName).ToList();
+                List<SelectListItem> sl = result.ConvertAll(a =>
+                {
+                    return new SelectListItem()
+                    {
+                        Text = a.UserName,
+                        Value = a.ID.ToString()
+                    };
+                });
+                return new SelectList(sl, "Value", "text");
+            }
+        }
+    }
+}
